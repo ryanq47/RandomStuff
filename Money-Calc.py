838ae0468c137d579e94765a062a0f47a37eb0c8
Ryan Kleffman <ryankleffman47@gmail.com>
2022-06-04 16:45:35 -0500

    Money-Calc
    Small update to include common additional fields

--- Money-Calc.py
+++ Money-Calc.py
@@ -1,18 +1,33 @@
 # Money Calc
 import math
 
+
 print('Enter PayCheck Amount')
 paycheck = int(input())
 
 print('Enter Investment amount')
 investment=int(input())
+
 print('Enter crypto amount')
 crypto=int(input())
 
+print('Enter savings amount')
+savings=int(input())
+
+
+print('Enter Parking per day')
+parking=int(input())
+print('How many days a week do you pay for parking?')
+parkingdays=int(input())
+
+parkingtotal=parking * parkingdays
+
+
+
 #perday $
 
-perday = (paycheck - (crypto + investment)) / 14
+perday = (paycheck - (crypto + investment + parkingtotal + savings)) / 14
 
 
-print('Your per day spending is:')
-print("$",perday)
+
+print("You can spend up to $",perday,"a day, and not go broke")
